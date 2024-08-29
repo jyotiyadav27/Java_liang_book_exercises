4da1e63b397478d5602e0e84810df55898708ef5
Jyoti Yadav <8717260+jyotiyadav27@users.noreply.github.com.>
2024-08-29 21:02:29 +0530

    Chapter 21 examples

---
+++ ch_21/countOccurenceWords.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class countOccureneceWords{
+    public static void main(String[] args){
+        Map<String, Integer> m = new TreeMap<>();
+        String s = "life is amazing or what is life";
+        String[] s1= s.split(" ");
+        for(String str:s1){
+            if(!m.containsKey(str)){
+                m.put(str,1);
+
+            } else {
+                int value = m.get(str);
+                value++;
+                m.put(str,value);
+            }
+        }
+        Set<Map.Entry<String, Integer>> entrySet = m.entrySet();
+        for(Map.Entry<String, Integer> e1:entrySet){
+            System.out.println(e1.getKey()+" "+e1.getValue());
+        }
+
+
+    }
+}
